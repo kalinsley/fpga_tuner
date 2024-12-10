0cf93a009e60bac222c0910b1644bf2c7a9528a6
Kai Linsley <98914767+kalinsley@users.noreply.github.com>
2024-12-10 15:52:28 -0800

    Delete lab-8-summative-assessment-kai-slug-main directory

--- lab-8-summative-assessment-kai-slug-main/util/get_filelist.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("filelist.json") as filelist:
-    files = json.load(filelist)["files"]
-    for f in files:
-        print(f,end=" ")
-    print()
--- lab-8-summative-assessment-kai-slug-main/util/get_top.py
+++
@@ -1,4 +0,0 @@
-import json
-
-with open("filelist.json") as filelist:
-    print(json.load(filelist)["top"])
